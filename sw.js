6873fb1dde10d47041628766bc98b6a8af7c99ae
= <=>
2023-07-01 18:01:00 +0900

    通知イベント検知テスト

--- sw.js
+++ sw.js
@@ -72,13 +72,11 @@ self.addEventListener('notificationclick', (event) => {
     event.waitUntil(clients.matchAll({
         type: "window"
     }).then((clientList) => {
+        event.notification.close();
         for (const client of clientList) {
             if ('focus' in client)
                 return client.focus();
         }
-
-        if (clients.openWindow)
-            return clients.openWindow('./');
     }));
 
     //clients.openWindow('https://www.google.com/')
